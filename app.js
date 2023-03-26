7e82d5e19946360238581c219770433d3c9ba14e
PassG3n <62288874+PassG3n@users.noreply.github.com>
2023-03-26 13:50:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 let special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '?', '/'];
 
@@ -8,6 +9,7 @@ function generatePassword() {
     let passwordLength = document.getElementById('passwordLength').value;
     let includeSpecial = document.getElementById('includeSpecial').checked;
     let includeNumbers = document.getElementById('includeNumbers').checked;
+    let includeNumbers = document.getElementById('includeUpper').checked;
 
     let password = [];
 
@@ -19,8 +21,11 @@ function generatePassword() {
         } else if (choose == 1 && includeNumbers) {
             password.push(number[Math.floor(Math.random() * number.length)]);
         } else if (choose == 2 && includeSpecial) {
+            password.push(special[Math.floor(Math.random() * upper.length)]);
+        }
+        else if (choose == 2 && includeUpper) {
             password.push(special[Math.floor(Math.random() * special.length)]);
-        } else {
+        }else {
             i--;
         }
         
